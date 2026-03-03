c0c097e86786d0f44da1938f9d1b1c8d09da87fa
Gundy05 <sgundy05@gmail.com>
2026-03-03 17:41:19 -0600

    Added Assignment 2

---
+++ src/TaskForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function TaskForm() {
+  const [task, setTask] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('New Task:', task);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Task Name"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default TaskForm;
---
+++ src/UserInfo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class UserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Seth',
+      profession: 'React Developer',
+    };
+  }
+
+  render() {
+    const luckyNumber = Math.floor(Math.random() * 10) + 1;
+
+    return (
+      <div>
+        <h2>User Information</h2>
+        <p>Name: {this.state.name}</p>
+        <p>Profession: {this.state.profession}</p>
+        <p>Your lucky number is {luckyNumber}</p>
+
+        <button onClick={this.props.handleClick}>Show Alert</button>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
